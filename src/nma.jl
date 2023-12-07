fa97b7570d813da349172d151f7c03e8b41d0b4d
drizk1 <rizkytennis@gmail.com>
2023-12-06 18:29:49 -0600

    added strip_non_alphanum arg

---
+++ src/nma.jl
@@ -0,0 +1,31 @@
+# Source: <http://saifmohammad.com/WebPages/SCL.html#NMA>
+# This is a list of 44 English negators, modals, and adverbs, presented as DataFrame with 44 rows and 2 variables - word(s) and modifier.
+# <http://saifmohammad.com/WebPages/SCL.html#NMA>
+
+"""
+$docstring_nma_words
+"""
+nma_words = DataFrame(
+    word = [
+        "cannot", "could not", "did not", "does not", "had no", 
+        "have no", "may not", "never", "no", "not", 
+        "nothing", "was no", "was not", "will not", "would not", 
+        "can", "could", "may", "might", "must", 
+        "should", "would", "certainly", "especially", "extremely", 
+        "fairly", "highly", "increasingly", "less", "more", 
+        "most", "much", "much more", "particularly", "pretty", 
+        "probably", "quite", "rather", "really", "relatively", 
+        "so", "too", "very", "very very"
+    ],
+    modifier = [
+        "negator", "negator", "negator", "negator", "negator", 
+        "negator", "negator", "negator", "negator", "negator", 
+        "negator", "negator", "negator", "negator", "negator", 
+        "modal", "modal", "modal", "modal", "modal", 
+        "modal", "modal", "adverb", "adverb", "adverb", 
+        "adverb", "adverb", "adverb", "adverb", "adverb", 
+        "adverb", "adverb", "adverb", "adverb", "adverb", 
+        "adverb", "adverb", "adverb", "adverb", "adverb", 
+        "adverb", "adverb", "adverb", "adverb"
+    ]
+)
